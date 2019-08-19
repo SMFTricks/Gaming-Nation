db722fc25ac63df87dbba1f9e080ce3d22311bc8
Diego Andrés <diegoandres_cortes@outlook.com>
2019-08-19 14:28:47 -0500

    Add files via upload

---
+++ languages/Settings.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.0; Settings
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Gaming Nation Theme.<br /><br />Author: <a href="http://smftricks.com">The SMF Tricks Team</a>';
+
+?>
---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,46 @@
+<?php
+
+
+$txt['st_sticky_topic'] = 'Sticky Topics';
+$txt['st_normal_topic'] = 'Normal Topics';
+$txt['st_separate_sticky_locked'] = 'Separate sticky topics';
+$txt['st_separate_sticky_locked_desc'] = 'With this, you can separate the sticky topics and your normal topics';
+
+$txt['current_theme'] = 'Theme settings';
+
+$txt['st_footer_image'] = 'Footer image URL';
+$txt['st_mini_logo'] = 'Mini Logo image URL';
+
+$txt['st_facebook_username'] = 'Facebook username';
+$txt['st_twitter_username'] = 'Twitter username';
+$txt['st_youtube_username'] = 'Youtube username';
+$txt['st_instagram_username'] = 'Instagram username';
+$txt['st_gplus_username'] = 'Google Plus username';
+$txt['st_rss_url'] = 'RSS URL';
+$txt['st_social_desc'] = 'Write the username or the id to show the button';
+
+$txt['st_enable_avatars'] = 'Enable the avatars';
+$txt['st_enable_avatars_boards'] = 'Enable avatar on board';
+$txt['st_enable_avatars_topics'] = 'Enable avatar on topic list';
+$txt['st_enable_avatars_recent'] = 'Enable avatar on recent posts stats';
+
+$txt['st_enable_carousel'] = 'Enable carousel';
+$txt['st_carousel_speed'] = 'Carousel speed';
+$txt['st_carousel_speed_desc'] = 'Speed in miliseconds, use 1000 for 1 second.';
+$txt['st_carousel_img1'] = 'Image 1';
+$txt['st_carousel_img2'] = 'Image 2';
+$txt['st_carousel_img3'] = 'Image 3';
+$txt['st_carousel_title1'] = 'Title 1';
+$txt['st_carousel_title2'] = 'Title 2';
+$txt['st_carousel_title3'] = 'Title 3';
+$txt['st_carousel_text1'] = 'Text 1';
+$txt['st_carousel_text2'] = 'Text 2';
+$txt['st_carousel_text3'] = 'Text 3';
+$txt['st_carousel_link1'] = 'Link 1';
+$txt['st_carousel_link2'] = 'Link 2';
+$txt['st_carousel_link3'] = 'Link 3';
+$txt['st_carousel_index'] = 'Allow only on index';
+$txt['st_previous'] = 'Previous';
+$txt['st_next'] = 'Next';
+
+$txt['st_allrights'] = 'All rights reserved';
